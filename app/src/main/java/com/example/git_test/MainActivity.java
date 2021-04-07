8febbfa87a7f917f7c3150373a2600f444bf0280
Aduba <likppi100@naver.com>
2021-04-07 11:21:23 +0900

    어라? 로봇이 손을 댔네? 나도 고칠거 있다

--- app/src/main/java/com/example/git_test/MainActivity.java
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     //안녕? 나는 로봇
     //인간이 수정하였다.
     //이간이 수정하였다.2
+    //어라? 로봇이 손을 댔네? 나도 고칠거 있다
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
